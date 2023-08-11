2fc8aaee15297e0b928d704577ef4669b1a19a3f
Artem Soloviev <soloviev.artem@gmail.com>
2023-08-11 10:58:01 +0300

    Initial Commit

---
+++ TestAppSpar/TestAppSparApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppSparApp.swift
+//  TestAppSpar
+//
+//  Created by Artem Soloviev on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppSparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
